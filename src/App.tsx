449310b88bef4035f0a7bfb7315f2ebc6ad6020a
kingchobo <kingchobo2022@gmail.com>
2025-11-30 15:49:10 +0900

    2025-11-30 2nd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
+import { useState } from "react"
+
 function App() {
+
+  const [newTitle, setNewTitle] = useState("");
+  const [adding, setAdding] = useState(false);
+
+  // Todo 추가
+  const handleAddTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    //console.log(newTitle);
+    const title = newTitle.trim();
+    if (!title) return;
+    setAdding(true);
+  }
 
   return (
     <main className="min-h-screen flex bg-slate-100 flex items-center justify-center p-4">
       <div className="w-full max-w-xl bg-white rounded-2xl shadow-lg p-6">
         <h1 className="text-2xl font-bold mb-4 text-center text-slate-800">Firebase Todo List</h1>
-        <form className="flex gap-2 mb-4">
+        <form className="flex gap-2 mb-4" onSubmit={handleAddTodo}>
           <input type="text" 
+
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
           className="flex-1 border border-slate-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="할 일을 입력하세요." />
-          <button className="px-4 py-2 rounded-lg bg-blue-600 text-white font-medium 
-          disabled:opacity-60 disabled:cursor-not-allowed hover:bg-blue-700 transition">추가</button>
+          <button 
+          disabled={adding}
+          className="px-4 py-2 rounded-lg bg-blue-600 text-white font-medium 
+          disabled:opacity-60 disabled:cursor-not-allowed hover:bg-blue-700 transition">
+            {adding ? '추가 중...' : '추가'}
+          </button>
         </form>
       </div>
     </main>
